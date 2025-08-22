bd8fce1815f17bd4f9036e48a8ead5febad6ae2e
Juan Francisco <juan.maldonado.leon@gmail.com>
2025-08-21 21:44:47 -0400

    Módulo de estudiantes

---
+++ src/estudiantes/dto/create-estudiante.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateEstudianteDto {
+  @ApiProperty({ default: 'Juan' })
+  public nombre: string;
+
+  @ApiProperty({ default: 'Maldonado' })
+  public apellidos: string;
+
+  @ApiProperty({ default: 36 })
+  public edad: number;
+
+  @ApiProperty({ default: 'juan@mail.com' })
+  public email: string;
+
+  @ApiProperty({ default: 'Ing. Informatico' })
+  public profesion: string;
+}
