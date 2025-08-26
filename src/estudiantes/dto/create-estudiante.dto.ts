808479242288f91a24a7ce7974d9713376dd6898
Gabriel-jns <jones.gabriel.a4@gmail.com>
2025-08-26 18:53:03 -0400

    Update create-estudiante.dto.ts
    Cambios de prueba

--- src/estudiantes/dto/create-estudiante.dto.ts
+++ src/estudiantes/dto/create-estudiante.dto.ts
@@ -1,10 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateEstudianteDto {
-  @ApiProperty({ default: 'Juan' })
+  @ApiProperty({ default: 'Gabriel' })
   public nombre: string;
 
-  @ApiProperty({ default: 'Maldonado' })
+  @ApiProperty({ default: 'Jones' })
   public apellidos: string;
 
   @ApiProperty({ default: 36 })
@@ -15,4 +15,6 @@ export class CreateEstudianteDto {
 
   @ApiProperty({ default: 'Ing. Informatico' })
   public profesion: string;
+
+  fechaNacimiento: Date;
 }
